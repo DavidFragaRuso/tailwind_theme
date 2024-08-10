7d426f9265e74f196a8ed275c8d6568aeef347ce
David Fraga <david.fraga.bcn@gmail.com>
2024-08-10 20:00:11 +0200

    Added Footer template

--- footer.php
+++ footer.php
@@ -9,22 +9,43 @@
  * @package tailwind_theme
  */
 
+global $theme_customizer;
+
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tailtheme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tailtheme' ), 'WordPress' );
+<footer id="colophon" class="site-footer">
+	<div class="container">
+		<div class="border-t-2 border-primary py-8 flex flex-wrap">
+			<div class="footer-widget px-4 mb-8">
+				<?php 
+				if( has_custom_logo() ) {
+					the_custom_logo();
+				} else {
+					?>
+					<span class="footer-site-title text-primary block mb-4"><?php echo bloginfo('name'); ?><br/>
+					<?php echo bloginfo('description') ?></span>
+					<?php
+				}
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tailtheme' ), 'tailtheme', '<a href="https://mi-web.com">David Fraga</a>' );
-				?>
-		</div><!-- .site-info -->
+				<?php $theme_customizer->tailtheme_render_rrss_links(); ?>
+			</div>
+			<div class="footer-widget px-4 mb-8">
+				<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+					<?php dynamic_sidebar( 'sidebar-2' ); ?>
+				<?php endif; ?>
+			</div>
+			<div class="footer-widget px-4 mb-8">
+				<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
+					<?php dynamic_sidebar( 'sidebar-3' ); ?>
+				<?php endif; ?>	
+			</div>
+			<div class="footer-widget px-4 mb-8">
+				<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+					<?php dynamic_sidebar( 'sidebar-4' ); ?>
+				<?php endif; ?>	
+			</div>
+		</div>
+	</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
